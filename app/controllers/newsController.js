a8e4939ed1098245ed1e0992ec57b6702b858f93
kevintatou <kevintatou94@gmail.com>
2016-02-19 13:46:56 +0100

    fixed news sections

--- app/controllers/newsController.js
+++ app/controllers/newsController.js
@@ -9,4 +9,17 @@ $http({
 	    $scope.data = datan;
 	   
 	  });
+      
+      
+$http({
+		method: 'GET',
+		url: apiURL+'user/1/posts'
+	}).then(function successCallback(response) {
+		var datan = response.data;
+		var counter = datan.length;
+	    $scope.posts = datan;
+	   console.log(datan);
+	  });
+
+
 });
