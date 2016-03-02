5904f3be66dc202d1d3c33a5a74a86bcabbb093f
Jesper <jesper@jesper.se>
2016-03-02 11:49:15 +0100

    hej

--- app/app.js
+++ app/app.js
@@ -3,32 +3,32 @@ var app = angular.module('app', ['ngRoute']);
 var apiURL = 'http://localhost:8000/api/';
 
 app.config(function($locationProvider, $routeProvider) {
-	$routeProvider
+    $routeProvider
 
-	.when('/', {
-		templateUrl: 'views/home.html',
-		controller: 'homeController'
-	})
-	.when('/profile', {
-		templateUrl: 'views/profile.html',
-		controller: 'profileController'
-	})
-	.when('/news', {
-		templateUrl: 'views/news.html',
-		controller: 'newsController'
-	})
-    .when('/courses', {
-		templateUrl: 'views/courses.html',
-		controller: 'coursesController'
-	})
-    .when('/course', {
-		templateUrl: 'views/course.html',
-		controller: 'courseController'
-	})
-    .when('/create', {
-		templateUrl: 'views/create.html',
-		controller: 'createController'
-	})
-	//.otherwise({ redirectTo: '/' });
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController'
+        })
+        .when('/profile', {
+            templateUrl: 'views/profile.html',
+            controller: 'profileController'
+        })
+        .when('/news', {
+            templateUrl: 'views/news.html',
+            controller: 'newsController'
+        })
+        .when('/courses', {
+            templateUrl: 'views/courses.html',
+            controller: 'coursesController'
+        })
+        .when('/course', {
+            templateUrl: 'views/course.html',
+            controller: 'courseController'
+        })
+        .when('/create', {
+            templateUrl: 'views/create.html',
+            controller: 'createController'
+        })
+	.otherwise({ redirectTo: '/' });
 });
 
