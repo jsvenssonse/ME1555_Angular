fc5363e83b4e5ea09df094a09f2a1101a4a2a6ea
Jesper <jesper@jesper.se>
2016-03-02 13:47:17 +0100

    courese

--- app/controllers/courseController.js
+++ app/controllers/courseController.js
@@ -1,11 +1,14 @@
-app.controller('courseController', function($scope, $http) {
+app.controller('courseController', function ($scope, $http) {
+
 $http({
 		method: 'GET',
-		url: apiURL+'posts'
+		url: apiURL+'course/1'
 	}).then(function successCallback(response) {
 		var datan = response.data;
 		var counter = datan.length;
-	    $scope.data = datan;
-	   
+        $scope.data = datan;
+        $scope.steps = datan.steps;
+       
+        console.log(datan);
 	  });
 });
