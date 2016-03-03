e43ae3308ef649eb12d7fd104ac26d8bf0ae164e
kevintatou <kevintatou94@gmail.com>
2016-03-03 18:50:55 -0500

    testing

--- app/controllers/coursesController.js
+++ app/controllers/coursesController.js
@@ -6,22 +6,10 @@ app.controller('coursesController', function($scope, $http) {
         
         $scope.filters = {};
         
-        var data = [{
-            categories: [
-                {name : "Programming", title: "Learn C++"},
-                {name : "Science", title: "Learn nuclear fission"},
-                {name : "Plan", title: [
-                   "Trip to Paris", 
-                   "Start a company",
-                   "Get sixpack abs"
-                ]}
-            ]
-        }]
         
+        $scope.categories = response.data;
         
-        $scope.categories = data[0].categories;
-        
-        $scope.mo = console.log(data[0].concat(categories));
+        $scope.mo = console.log(response.data);
         
         
 //    angular.element(document).ready(function () {
