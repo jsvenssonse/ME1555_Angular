4bef277046e72c29fff8b31be11bfabe9871c822
Alex Hoeing <lhoeing@msn.com>
2016-03-04 15:39:13 +0100

    Google sign in

---
+++ app/config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'clientID':'92160089603-ur447dtro0acdr00odolp3b8341osmjh.apps.googleusercontent.com',
+    'clientSecret':'wnA-0rD4qB5bRej4UPNSf560',
+    'callbackURL':'/auth/google/callback'
+}
